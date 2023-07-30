70c3c9fe9749e4f1ec9de18f91143cff88609cc4
itay moshel <itay@itays-MacBook-Pro.local>
2023-07-30 17:05:28 +0300

    Exercises COMPLETE.

---
+++ Week4/Day1/Exercises/ExerciseGold.sql
@@ -0,0 +1,11 @@
+-- SELECT first_name, last_name, birth_date FROM students
+-- ORDER BY last_name
+-- LIMIT 4;
+-- SELECT first_name, last_name, birth_date FROM students
+-- ORDER BY birth_date DESC
+-- LIMIT 1;
+SELECT first_name, last_name, birth_date FROM students
+ORDER BY id
+OFFSET 2
+LIMIT 3;
+
