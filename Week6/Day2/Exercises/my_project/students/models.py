3746c918b7a72cd35847d171f868fa600ca0b275
itay moshel <itay@itays-MacBook-Pro.local>
2023-08-15 05:26:03 +0300

    W6D2Exercise

---
+++ Week6/Day2/Exercises/my_project/students/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.db import models
+from django.core.validators import EmailValidator
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(validators=[EmailValidator()])
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
